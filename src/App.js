63792f55ff7f063378b2428a00d8d3c7b5af12da
Emilio <emilio.orellana@fce.uncu.edu.ar>
2023-07-07 02:27:37 -0300

    BasicFunctionalityFinished
    Random quote gen con funcionalidad basica terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import quotes from './quotes.js';
+
+class RandomQuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    let r = Math.floor(Math.random() * quotes.length);
+    this.state = {
+      randomNumber: r,
+      quote: quotes[r].quote,
+      author: quotes[r].author,      
+    }
+    this.handleClick = this.handleClick.bind(this);
+
+  };  
+
+  componentDidMount() {
+    this.generateRandomQuote();
+  }
+
+  generateRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const { quote, author } = quotes[randomIndex];
+    this.setState({
+      randomNumber: randomIndex,
+      quote: quote,
+      author: author,
+    });
+  }
+
+  handleClick() {
+    this.generateRandomQuote();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+    
+          <p id = "quote">
+            "{this.state.quote}"
+          </p>
+          <p id = "author">
+            - {this.state.author}
+          </p>
+
+          <button 
+            id = "new-quote" 
+            onClick = {this.handleClick}>
+              New quote
+          </button>
+
+        </header>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RandomQuoteMachine />
   );
 }
 
---
+++ src/quotes.js
@@ -0,0 +1,212 @@
+const quotes = [
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", 
+    author: "Nelson Mandela"
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.", 
+    author: "Walt Disney"
+  },
+  {
+    quote: "If life were predictable it would cease to be life, and be without flavor.", 
+    author: "Eleanor Roosevelt"
+  },
+  {
+    quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", 
+    author: "James Cameron"
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.", 
+    author: "John Lennon"
+  },  
+  {
+    quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.", 
+    author: "Mother Teresa"
+  },
+  {
+    quote: "Don’t wait. The time will never be just right.", 
+    author: "Napoleon Hill"
+  },
+  {
+    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.", 
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "Whoever is happy will make others happy too.", 
+    author: "Anne Frank"
+  },
+  {
+    quote: "You will face many defeats in life, but never let yourself be defeated.", 
+    author: "Maya Angelou"
+  },
+  {
+    quote: "A positive mind towards everything will give you a happier life." , 
+    author: "Unknown"
+  },
+  {
+    quote: "The best way to predict the future is to create it.", 
+    author: "Abraham Lincoln"
+  },
+  {
+    quote: "The only person you are destined to become is the person you decide to be.", 
+    author: "Ralph Waldo Emerson"
+  },
+  {
+    quote: "Live your life and forget your age.", 
+    author: "Jean Paul"
+  }, //index 13
+  {
+    quote: "Success is not a destination, it’s a journey.", 
+    author: "Zig Ziglar"
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+  },
+  {
+    quote: "Life is 10% what happens to you and 90% how you react to it.",
+    author: "Charles R. Swindoll"
+  },
+  {
+    quote: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse"
+  },
+  {
+    quote: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    author: "Maya Angelou"
+  },
+  {
+    quote: "Every child is an artist. The problem is how to remain an artist once he grows up.",
+    author: "Pablo Picasso"
+  },
+  {
+    quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
+    author: "Amelia Earhart"
+  },
+  {
+    quote: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky"
+  },
+  {
+    quote: "Whether you think you can or you think you can’t, you’re right.",
+    author: "Henry Ford"
+  },
+  {
+    quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover.",
+    author: "Mark Twain"
+  },
+  {
+    quote: "The two most important days in your life are the day you are born and the day you find out why.",
+    author: "Mark Twain"
+  },
+  {
+    quote: "Certain things catch your eye, but pursue only those that capture the heart.",
+    author: "Ancient Indian Proverb"
+  },
+  {
+    quote: "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+    author: "Helen Keller"
+  },
+  {
+    quote: "If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.",
+    author: "Sheryl Sandberg" 
+  },
+  {
+    quote: "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
+    author: "Joshua J. Marine"
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington"
+  },
+  {
+    quote: "I didn’t fail the test. I just found 100 ways to do it wrong.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "In order to succeed, your desire for success should be greater than your fear of failure.",
+    author: "Bill Cosby"
+  },
+  {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+  },
+  {
+    quote: "A person who never made a mistake never tried anything new.",
+    author: " Albert Einstein"
+  },
+  {
+    quote: "The person who says it cannot be done should not interrupt the person who is doing it.",
+    author: "Confucius"
+  },
+  {
+    quote: "There are no traffic jams along the extra mile.",
+    author: "Roger Staubach"
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius"
+  },
+  {
+    quote: "Our lives begin to end the day we become silent about things that matter.",
+    author: "Martin Luther King Jr."
+  },
+  {
+    quote: "Do what you can, where you are, with what you have.",
+    author: "Teddy Roosevelt"
+  },
+  {
+    quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Will Durant"
+  },
+  {
+    quote: "If you do what you’ve always done, you’ll get what you’ve always gotten.",
+    author: "Tony Robbins"
+  },
+  {
+    quote: "Dreaming, after all, is a form of planning.",
+    author: "Gloria Steinem"
+  },
+  {
+    quote: "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
+    author: "Henry Ford"
+  },
+  {
+    quote: "Either write something worth reading or do something worth writing.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote: "You may be disappointed if you fail, but you are doomed if you don’t try.",
+    author: "Beverly Sills"
+  },
+  {
+    quote: "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.",
+    author: "Leonardo da Vinci"
+  },
+  {
+    quote: "Limitations live only in our minds.  But if we use our imaginations, our possibilities become limitless.",
+    author: "Jamie Paolinetti"
+  },
+  {
+    quote: "Too many of us are not living our dreams because we are living our fears.",
+    author: "Les Brown"
+  },
+  {
+    quote: "Life is not measured by the number of breaths we take, but by the moments that take our breath away.",
+    author: "Maya Angelou"
+  },
+  {
+    quote: "You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.",
+    author: "Unknown"
+  },
+  {
+    quote: "You must be the change you wish to see in the world.",
+    author: "Unknown"
+  },
+  {
+    quote: "When I look to the mountain's peak, in my mind, I have already fallen. And so, I climb.",
+    author: "Unknown"
+  }
+]
+
+export default quotes;
